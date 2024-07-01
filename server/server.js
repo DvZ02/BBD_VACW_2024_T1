b60d9751a68e7bf4c562381cea8f02f3a6be6e06
DvZ <danielvzyl1469@gmail.com>
2024-07-01 11:27:37 +0200

    🎨 Server boiler plate

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketio = require('socket.io');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+const io = socketio(server);
+
+io.on('connection',  (socket) => {
+    console.log('New client connected');
+
+    //Most socket logic here
+});
