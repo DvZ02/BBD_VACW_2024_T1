e43d89f87aff7bcf781ad474d5770573a03183aa
DvZ <danielvzyl1469@gmail.com>
2024-07-02 12:20:31 +0200

    💄 Added wait message

--- client/controller/js/controller.js
+++ client/controller/js/controller.js
@@ -1,14 +1,16 @@
 const socket = new io('https://tilt-3596.onrender.com');
 
-async function signup(){
+function signup(){
     let username = document.getElementById("username").value;
     // console.log(username);
     socket.emit("RequestSignUp",JSON.stringify({playerUsername:username}));
 
-    await socket.on("RequestSignUpResult", async (data) =>{
+    socket.on("RequestSignUpResult", async (data) =>{
         let serverResponse = JSON.parse(data);
         if(serverResponse.result){
             // window.location.href = "lobby.html";
+            username.value = "";
+            document.getElementsByClassName("waiting").style.display = "block";
         }
     });
 }
