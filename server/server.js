856a654ffebbcf5c82934d9da36c60a1eae28e3d
DvZ <danielvzyl1469@gmail.com>
2024-07-03 16:20:00 +0200

    Win  bug

--- server/server.js
+++ server/server.js
@@ -145,7 +145,7 @@ io.on('connection',  (socket) => {
     })
 
     socket.on("BallDistance", (data) =>{
-        console.log(playersDB[data.player]);
-        playersDB[data.player].score = Math.round(data.distance, 2);
+        if(playersDB[data.player] != undefined)
+            playersDB[data.player].score = Math.round(data.distance, 2);
     });
 });
